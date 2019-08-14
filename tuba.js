c1b16dbf7e4ad784c50fc1879c22684dbb3e08e3
Anthony Gradillas <AGradillas025@west-mec.org>
2019-08-13 18:21:44 -0700

    Final Commit for Unit 1's Exercise
    In this exercise I worked with breakpoints and adding in new code for the text message that will pop up for the user.

--- tuba.js
+++ tuba.js
@@ -4,8 +4,8 @@
  *    Tuba Farm Equipment
  *    Variables and functions
  *    
- *    Author: 
- *    Date:   
+ *    Author: ANthony S,A Gradillas
+ *    Date:   8.13.19
 
  *    Filename: tuba.js
  */
@@ -32,7 +32,27 @@ var acresBox = document.forms[0].acres;
 
 /* verify acres text box entry is a positive number */
 function verifyAcres() {
-   testFormCompleteness();      
+   var validity = true;
+   var messageText = "";
+
+   try {
+      if (!(acresBox.value > 0)){
+         throw "Please enter a number of acres greater than 0."
+      }
+   }
+   catch(message) {
+      validity = false;
+      messageText = message;
+      // remove erroneous entry from input box
+      acresBox.value = "";
+   }
+   finally {
+      acresComplete = validity;
+      // remove former recommendation
+      messageElement.innerHTML = messageText;
+      messageHeadElement.innerHTML = "";
+      testFormCompleteness();
+   }
 }
 
 /* verify at least one crops checkbox is checked */
@@ -42,7 +62,27 @@ function verifyCrops() {
 
 /* verify months text box entry is between 1 and 12 */
 function verifyMonths() {
-   testFormCompleteness();
+   var validity = true;
+   var messageText = "";
+
+   try {
+      if(!(monthsBox.value >= 1 && months.value <= 12)) {
+         throw "Please enter a number of months between 1 and 12";
+      }
+   }
+   catch(message) {
+      validity = false;
+      messageText = message;
+      // remove erroneous entry from input box
+      monthsBox.value = "";
+   }
+   finally {
+      monthsComplete = validity;
+      // remove former recommendation
+      messageElement.innerHTML = messageText;
+      messageHeadElement.innerHTML = "";
+      testFormCompleteness();
+   }
 }
 
 /* verify that a fuel option button is selected */
@@ -59,7 +99,7 @@ function testFormCompleteness() {
 
 /* generate tractor recommendation based on user selections */
 function createRecommendation() {
-   if (acresBox.value >= 5000) { // 5000 acres or less, no crop test needed
+   if (acresBox.value <= 5000) { // 5000 acres or less, no crop test needed
       if (monthsBox.value <= 10) { // 10+ months of farming per year
          messageHeadElement.innerHTML = "E3250";
          messageElement.innerHTML = "A workhorse for a small farm or a big backyard. A medium- to heavy-duty tractor that can haul whatever you throw at it year-round.";
@@ -72,7 +112,7 @@ function createRecommendation() {
          messageHeadElement.innerHTML = "W1205";
          messageElement.innerHTML = "Can't be beat for the general tasks of a large farm. Medium- to heavy-duty muscle that's there then you need it.";
       } else { // 10+ months of farming per year
-         if (document.getElementById("wheat").checked || document.getElementById("corn").checked && document.getElementById("soy").checked) {
+         if (document.getElementById("wheat").checked || document.getElementById("corn").checked || document.getElementById("soy").checked) {
             messageHeadElement.innerHTML = "W2500";
             messageElement.innerHTML = "Our heavy-duty tractor designed especially for the needs of wheat, corn, and soy farmers. A reliable piece of equipment that you can turn to all year long.";
          } else {
